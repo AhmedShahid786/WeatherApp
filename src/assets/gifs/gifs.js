468ff77c144e90e49fa0ad1896ea2c7d2c420b99
Ahmed Shahid <a.razashahid19@gmail.com>
2024-09-16 20:24:34 +0500

    Import export icons

--- src/assets/gifs/gifs.js
+++ src/assets/gifs/gifs.js
@@ -4,12 +4,33 @@ import feelsLikeGif from "./feelsLike.gif"
 import sunriseGif from "./sunrise.gif"
 import moonriseGif from "./moonrise.gif"
 import wind from "./wind.gif"
+import FormItemLabel from "antd/es/form/FormItemLabel"
 
-export {
-    humidityGif,
-    pressureGif,
-    feelsLikeGif,
-    sunriseGif,
-    moonriseGif,
-    wind
-}
+const gifs = [
+  {
+    gif: humidityGif,
+    label: "Humidity",
+  },
+  {
+    gif: pressureGif,
+    label: "Pressure",
+  },
+  {
+    gif: feelsLikeGif,
+    label: "Feels Like",
+  },
+  {
+    gif: sunriseGif,
+    label: "Sunrise",
+  },
+  {
+    gif: moonriseGif,
+    label: "Moonrise",
+  },
+  {
+    gif: wind,
+    label: "Wind",
+  }
+];
+
+export default gifs
