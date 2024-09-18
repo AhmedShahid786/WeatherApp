60879180455fbd94dad98ea54dc0967c13d21a1f
Ahmed Shahid <a.razashahid19@gmail.com>
2024-09-18 18:25:30 +0500

    Theme Colors Apllied

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -1,16 +1,28 @@
 import { useContext } from "react";
-import { WeatherContext } from "../contexts/WeatherContext";
-
+import { themeContext } from "../contexts/ThemeContext";
 
 export const AdditionalInfo = ({pic, label, value}) => {
+
+  const {theme} = useContext(themeContext)
+
   return (
     <div className="testBorder w-2/4 flex mb-4 mt-4">
       <div className="w-1/4 flex justify-end">
         <img src={pic} alt="" className="w-20 h-20" />
       </div>
       <div className="testBorder2 w-3/4 flex flex-col justify-around items-start text-sub">
-        <p className="opacity-60 text-lg">{label}</p>
-        <p className="text-4xl">{value}</p>
+        <p
+          className={`opacity-60 text-base
+          ${theme === "dark" ? "text-thirdD" : "text-thirdL"}`}
+        >
+          {label}
+        </p>
+        <p
+          className={`text-3xl
+          ${theme === "dark" ? "text-fourthD" : "text-fourthL"}`}
+        >
+          {value}
+        </p>
       </div>
     </div>
   );
--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,22 +1,41 @@
 import { useContext } from "react";
 import { WeatherContext } from "../contexts/WeatherContext";
+import { themeContext } from "../contexts/ThemeContext";
 
 export const WeatherData = () => {
+  const {theme} = useContext(themeContext)
   const { currentWeather, city } = useContext(WeatherContext);
   const {desc, temp} = currentWeather
   return (
     <div className="w-2/4">
       <div className="testBorder3 w-full py-8 flex flex-col items-start justify-start">
-        <p className="text-5xl mb-4">
+        <p
+          className={`text-5xl mb-4
+          ${theme === "dark" ? "text-fourthD" : "text-fourthL"}
+          `}
+        >
           {city ? city[0].toUpperCase() + city.slice(1) : ""}
         </p>
-        <p className="text-lg">
+        <p
+          className={`text-lg
+          ${theme === "dark" ? "text-thirdD" : "text-thirdL"}`}
+        >
           {desc ? desc[0].toUpperCase() + desc.slice(1) : ""}
         </p>
       </div>
       <div className="w-full flex justify-start items-center">
-        <p className="text-9xl">{temp ? Math.round(temp - 273.15) : ""}</p>
-        <span className="text-5xl mb-auto">{"\u00B0"}</span>
+        <p
+          className={`text-9xl
+          ${theme === "dark" ? "text-fourthD" : "text-fourthL"}`}
+        >
+          {temp ? Math.round(temp - 273.15) : ""}
+        </p>
+        <span
+          className={`text-5xl mb-auto
+          ${theme === "dark" ? "text-thirdD" : "text-thirdL"}`}
+        >
+          {"\u00B0"}
+        </span>
       </div>
     </div>
   );
