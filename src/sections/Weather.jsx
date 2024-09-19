993fb18cf92a2e64a48188ce052237d2bd4159e6
Ahmed Shahid <a.razashahid19@gmail.com>
2024-09-19 17:20:08 +0500

    Customize theme of tabbars

--- src/sections/Weather.jsx
+++ src/sections/Weather.jsx
@@ -1,30 +1,61 @@
 import { Tabs } from "antd";
+import { useContext, useState } from "react";
+import { themeContext } from "../contexts/ThemeContext";
 import WeatherToday from "./subSections/WeatherToday";
-const onChange = (key) => {
-  console.log(key);
-};
-const items = [
-  {
-    key: "1",
-    label: "Today",
-    children: <WeatherToday />,
-  },
-  {
-    key: "2",
-    label: "Tomorrow",
-    children: "Content of Tab Pane 2",
-  },
-  {
-    key: "3",
-    label: "Next 7 Days",
-    children: "Content of Tab Pane 3",
-  },
-];
 
-function Weather() {
+
+const Weather = () => {
+  const {theme} = useContext(themeContext)
+  const [activeKey, setActiveKey] = useState("1");
+  
+  const items = [
+    {
+      key: "1",
+      label: (
+        <span style={{ color: activeKey === "1" ? "#00FFF5" : "#FF2E63"  }}>
+          Today
+        </span>
+      ),
+      children: <WeatherToday />,
+    },
+    {
+      key: "2",
+      label: (
+        <span style={{ color: activeKey === "2" ? "#00FFF5" : "#FF2E63"  }}>
+          Tomorrow
+        </span>
+      ),
+      children: "Content of Tab Pane 2",
+    },
+    {
+      key: "3",
+      label: (
+        <span style={{ color: activeKey === "3" ? "#00FFF5" : "#FF2E63"  }}>
+          Next 7 Days
+        </span>
+      ),
+      children: "Content of Tab Pane 3",
+    },
+  ];
+
+  const onChange = (key) => {
+    setActiveKey(key);
+  };
+
   return (
-    <Tabs defaultActiveKey="1" items={items} onChange={onChange} size="large" />
+    <Tabs
+      defaultActiveKey="1"
+      onChange={onChange}
+      className={
+        theme === "dark"
+          ? "custom-tabsD ant-tabs-tab-btn"
+          : "custom-tabsL ant-tabs-tab-btn"
+      }
+      items={items}
+      tabBarStyle={{ marginBottom: 0 }}
+      tabPaneStyle={{ paddingTop: 0 }}
+    />
   );
-}
+};
 
 export default Weather;
