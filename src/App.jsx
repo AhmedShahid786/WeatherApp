e1cbdec5196e3d365d6c1705ccc69a3fcae0362d
Ahmed Shahid <a.razashahid19@gmail.com>
2024-09-22 00:59:53 +0500

    Created routes to all pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,12 @@ import ThemeContextProvider, { themeContext } from "./contexts/ThemeContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MenuSec from "./sections/Menu"
 import Home from "./pages/Home";
+import Cities from "./pages/Cities"
+import Map from "./pages/Maps" 
+import Settings from "./pages/settings";
+import Profile from "./pages/Profile"
 import "./index.css";
+import Settings from "./pages/settings";
 
 function App() {
   return (
@@ -14,7 +19,10 @@ function App() {
             <MenuSec />
             <Route index path="/" element={<Home/>}/>
             <Route path="/" element={<Home/>}/>
-            <Route path="/cities" element={"a"}/>
+            <Route path="/cities" element={<Cities/>}/>
+            <Route path="/map" element={<Map/>}/>
+            <Route path="/settings" element={<Settings/>}/>
+            <Route path="/profile" element={<Profile/>}/>
           </Routes>
         </ThemeContextProvider>
       </WeatherContextProvider>
