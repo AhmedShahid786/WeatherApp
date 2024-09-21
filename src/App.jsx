2e2931bb0b69bcc9b2d5cec0134e19562cf5f4f8
Ahmed Shahid <a.razashahid19@gmail.com>
2024-09-22 00:39:42 +0500

    cities page

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
 import { WeatherContextProvider } from "./contexts/WeatherContext";
 import ThemeContextProvider, { themeContext } from "./contexts/ThemeContext";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MenuSec from "./sections/Menu"
 import Home from "./pages/Home";
 import "./index.css";
 
 function App() {
   return (
-    <
-    >
+    <BrowserRouter>
       <WeatherContextProvider>
         <ThemeContextProvider>
+          <Routes>
             <MenuSec />
-          <section>
-            <Home />
-          </section>
+            <Route index path="/" element={<Home/>}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/cities" element={"a"}/>
+          </Routes>
         </ThemeContextProvider>
       </WeatherContextProvider>
-    </>
+    </BrowserRouter>
   );
 }
 
