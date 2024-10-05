1466718d366e28116ee71c2a5293573dbfb5882d
Ahmed Shahid <a.razashahid19@gmail.com>
2024-10-05 17:16:27 +0500

    Used icons intstead of gifs

--- src/components/AdditionalInfo.jsx
+++ src/components/AdditionalInfo.jsx
@@ -1,26 +1,36 @@
 import { useContext } from "react";
 import { themeContext } from "../contexts/ThemeContext";
 
-export const AdditionalInfo = ({ pic, label, value }) => {
+export const AdditionalInfo = ({ icon, label, value }) => {
   const { theme } = useContext(themeContext);
 
   return (
     <div className=" w-2/4 flex mb-4 mt-4">
-      <div className="w-1/4 flex justify-end">
-        <img src={pic} alt="" className="w-16 h-16" />
+      <div className="w-1/4 flex items-center justify-center">
+        <img src={icon} alt="" className="w-8 h-8 mt-2" />
       </div>
       <div className="w-3/4 flex flex-col justify-around items-start text-sub">
         <p
           className={`text-base
-          ${theme === "dark" ? "text-thirdD" : "text-thirdL"}`}
+            ${theme === "dark" ? "text-thirdD" : "text-thirdL"}`}
         >
           {label}
         </p>
         <p
           className={`text-3xl
-          ${theme === "dark" ? "text-fourthD" : "text-fourthL"}`}
+              ${theme === "dark" ? "text-fourthD" : "text-fourthL"}`}
         >
           {value}
+          {label.includes("Feels Like") ? (
+            <span
+              className={`text-4xl mb-auto
+          ${theme === "dark" ? "text-fourthD" : "text-fourthL"}`}
+            >
+              {"\u00B0"}
+            </span>
+          ) : (
+            ""
+          )}
         </p>
       </div>
     </div>
