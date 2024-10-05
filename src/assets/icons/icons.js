1923f831b1ef0f4dd9b8cea951a0d559b92eaad6
Ahmed Shahid <a.razashahid19@gmail.com>
2024-10-05 17:15:15 +0500

    Added icons to display additional weather info

---
+++ src/assets/icons/icons.js
@@ -0,0 +1,40 @@
+import humidityIcon from "./humidity.png";
+import pressureIcon from "./pressure.png";
+import feelsLikeIcon from "./feelsLike.png";
+import sunriseIcon from "./sunrise.png";
+import sunsetIcon from "./sunset.png";
+import windIcon from "./wind.png";
+
+const additionalInfoIcons = [
+  {
+    icon: humidityIcon,
+    label: "Humidity",
+  },
+  {
+    icon: pressureIcon,
+    label: "Pressure",
+  },
+  {
+    icon: feelsLikeIcon,
+    label: "Feels Like",
+  },
+  {
+    icon: sunriseIcon,
+    label: "Sunrise",
+  },
+  {
+    icon: sunsetIcon,
+    label: "Moonrise",
+  },
+  {
+    icon: windIcon,
+    label: "Wind",
+  },
+];
+
+export default additionalInfoIcons;
+
+// https://www.freepik.com/icon/thermometer_11325002#fromView=search&page=2&position=36&uuid=78f0a1ab-d0ca-451a-918e-2a5c46053b1f
+// https://www.freepik.com/icon/sunrise-alt_6854024#fromView=search&page=1&position=1&uuid=2dedb9e4-00e9-415b-b614-f249f1c55e6a
+// https://www.freepik.com/icon/sunset_6854030#fromView=search&page=1&position=14&uuid=71477d73-d532-4f5e-b402-db1521a74ab4
+// https://www.freepik.com/icon/wind_10035265#fromView=search&page=1&position=58&uuid=2125d588-657c-4d93-912e-ca5df70559a8
