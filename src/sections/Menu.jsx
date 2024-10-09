2fee50ad595a04c45822fd59a945d4eb1f607490
Ahmed Shahid <a.razashahid19@gmail.com>
2024-10-10 00:43:05 +0500

    Menu design complete

--- src/sections/Menu.jsx
+++ src/sections/Menu.jsx
@@ -4,7 +4,14 @@ import { useNavigate } from "react-router";
 
 //? Context imports
 import { themeContext } from "../contexts/ThemeContext";
-import { citiesIcon, cloudD, mapIcon, menuIcon, profileIcon, settingsIcon } from "../assets/icons/icons";
+import {
+  citiesIcon,
+  cloudD,
+  mapIcon,
+  profileIcon,
+  settingsIcon,
+  thunderRain,
+} from "../assets/icons/icons";
 import { Link } from "react-router-dom";
 
 //? Ant design imports
@@ -33,7 +40,7 @@ const items = [
     key: "5",
     icon: profileIcon,
     label: "Profile",
-    path : "/profile"
+    path: "/profile",
   },
   {
     key: "4",
@@ -43,18 +50,16 @@ const items = [
   },
 ];
 
-
 const Menu = () => {
+  const { theme } = useContext(themeContext);
 
-    const {theme} = useContext(themeContext)
-
-    const navigate = useNavigate()
-    const handleClick = (e)=>{
-       const selectedItem = items.find((item) => item.key === e.key);
-       if (selectedItem) {
-         navigate(selectedItem.path);
-       }
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    const selectedItem = items.find((item) => item.key === e.key);
+    if (selectedItem) {
+      navigate(selectedItem.path);
     }
+  };
 
   //? AntD Component Code
   const [collapsed, setCollapsed] = useState(false);
@@ -63,20 +68,35 @@ const Menu = () => {
   };
   return (
     <div
-      className={`h-full w-[9dvw] flex flex-col items-center justify-start bg-transparent`}
+      className={`h-full w-[9dvw] flex flex-col items-center justify-start bg-transparent border-r-2
+        ${theme === "dark" ? "border-thirdD" : "border-thirdL"}`}
     >
       <div className="w-full h-1/6 flex items-center justify-center">
-        <img src={menuIcon} alt="" className="h-8 w-8 cursor-pointer" />
+        <img
+          src={thunderRain}
+          alt=""
+          className="h-16 w-16 mt-6 cursor-pointer"
+        />
       </div>
 
-      <div className="w-full h-4/6 flex flex-col items-center justify-evenly">
+      <div className="w-full h-4/6 flex flex-col items-center justify-center gap-6 my-auto">
         {items.map((item) => (
-          <Link to={item.path} key={item.key} className="w-full flex flex-col items-center justify-center">
-            <img src={item.icon} alt="" className="w-10 h-10" />
+          <Link
+            to={item.path}
+            key={item.key}
+            className="w-full flex flex-col items-center justify-center"
+          >
+            <img src={item.icon} alt="" className="w-8 h-8" />
+            <p
+              className={`font-customFont leading-none text-xs
+          ${theme === "dark" ? "text-fourthD" : "text-fourthL"}`}
+            >
+              {item.label}
+            </p>
           </Link>
         ))}
       </div>
     </div>
   );
-}
+};
 export default Menu;
