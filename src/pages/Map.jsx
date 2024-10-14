f0ca10896f7b470fd536a2e4f346466056d15fb6
Ahmed Shahid <a.razashahid19@gmail.com>
2024-10-15 01:00:12 +0500

    Rendered realtime map using leaflet js

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,7 +1,42 @@
-function Map () {
-  return (
-    <div>map</div>
-  )
-}
+import React, { useContext } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { WeatherContext } from "../contexts/WeatherContext";
 
-export default Map
+// Set the default icon for Leaflet markers
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+const Map = () => {
+  const {mapPosition} = useContext(WeatherContext)
+  
+  
+
+  return (
+    <MapContainer
+      center={mapPosition}
+      zoom={7}
+      style={{ height: "100vh", width: "100%" }}
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Marker position={mapPosition}>
+        <Popup>
+        You're Here
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
