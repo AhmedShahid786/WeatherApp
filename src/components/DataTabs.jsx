0f6e12fac8ade4058466c881579aeccc75a933e2
Ahmed Shahid <a.razashahid19@gmail.com>
2024-10-15 00:49:56 +0500

    Downloaded firebase and leaflet js

--- src/components/DataTabs.jsx
+++ src/components/DataTabs.jsx
@@ -1,14 +1,15 @@
 import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
-export default function DataTabs() {
-    const data = [
-      "Temperature",
-      "Humidity",
-      "Wind",
-      "Pressure",
-      "Sunrise",
-      "Sunrise"
-    ]
+export default function DataTabs({ handleCurrentDataKey, currentDataKey }) {
+  const data = [
+    "Temperature",
+    "Humidity",
+    "Wind",
+    "Pressure",
+    "Sunrise",
+    "Sunset",
+  ];
+
   return (
     <div className="w-full mt-4 flex items-center justify-start">
       <TabGroup>
@@ -16,6 +17,7 @@ export default function DataTabs() {
           {data.map((item) => (
             <Tab
               key={item}
+              onClick={() => currentDataKey != item ? handleCurrentDataKey(item.toLowerCase()) : ""}
               className="rounded-full border-2 border-thirdD py-0 px-2 text-sm font-semibold text-thirdD data-[selected]:border-fourthD data-[selected]:text-fourthD data-[selected]:border-fourthD data-[hover]:text-fourthD  data-[hover]:border-fourthD bg-transparent"
             >
               {item}
