c2196d818072d43e02a3345bc58af0cedcbaca2a
Ahmed Shahid <a.razashahid19@gmail.com>
2024-10-16 20:01:25 +0500

    Trying to solve deployement issue

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path"; // Import path to use absolute imports
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "src/components"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+    },
+  },
+});
