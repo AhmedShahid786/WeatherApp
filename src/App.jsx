bd68d9cbf93facef0667f45238f6fac712da9dfd
Ahmed Shahid <a.razashahid19@gmail.com>
2024-10-17 19:51:20 +0500

    Vercel issue solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { WeatherContextProvider } from "./contexts/WeatherContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Weather from "@pages/Weather";
-import Cities from '@pages/Cities';
-import Map from "@pages/map";
-import Profile from "@pages/Profile";
-import RootLayout from "@pages/RootLayout";
+import Weather from "./pages/Weather";
+import City from "./pages/City";
+import Map from "./pages/map";
+import Profile from "./pages/Profile";
+import RootLayout from "./pages/RootLayout";
 import "./index.css";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<RootLayout />}>
               <Route index element={<Weather />} />
-              <Route path="cities" element={<Cities/>} />
+              <Route path="cities" element={<City/>} />
               <Route path="map" element={<Map />} />
               <Route path="profile" element={<Profile />} />
             </Route>
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // Import path to use absolute imports
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@contexts": path.resolve(__dirname, "src/contexts"),
-    },
-  },
 });
