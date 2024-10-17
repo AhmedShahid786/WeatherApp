3414f8b7acae1b00c4ccd17ad906489c1babc6fb
Ahmed Shahid <a.razashahid19@gmail.com>
2024-10-17 20:48:16 +0500

    Deployement issue solve

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { WeatherContextProvider } from "./contexts/WeatherContext";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Weather from "./pages/Weather";
-import City from "./pages/City";
+import City from "./pages/City";  
 import Map from "./pages/map";
 import Profile from "./pages/Profile";
 import RootLayout from "./pages/RootLayout";
